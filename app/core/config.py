a0e032ab025b2dc5cf6b5fd909360041e5ebf551
waterlilynnn <april.programming@gmail.com>
2026-04-11 12:17:05 +0800

    fix config

--- app/core/config.py
+++ app/core/config.py
@@ -1,23 +1,25 @@
 import os
-from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
-
+# Load .env file ONLY in development
 ENV = os.getenv("ENV", "development")
+if ENV == "development":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+# ENV = os.getenv("ENV", "development") =
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_PORT = int(os.getenv("DB_PORT", "3306")) 
 DB_NAME = os.getenv("DB_NAME", "db_cennro")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "TEST_SECRETKEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
 
 SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
-SMTP_USER = os.getenv("SMTP_USER", "april.programming@gmail.com")
-SMTP_PASS = os.getenv("SMTP_PASS", "terb rbif lghk ucrw") 
-
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_USER = os.getenv("SMTP_USER", "cenro.cityoftagaytay@gmail.com")
+SMTP_PASS = os.getenv("SMTP_PASS", "veyl wcof dlvq angu")
