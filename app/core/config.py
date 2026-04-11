0e9e587df829381d6a31fff48d847aaf1c6e8ec4
waterlilynnn <april.programming@gmail.com>
2026-04-11 12:32:57 +0800

    fix config

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,7 @@ if ENV == "development":
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", "3306")) 
-DB_NAME = os.getenv("DB_NAME", "db_cennro")
+DB_NAME = os.getenv("DB_NAME", "db_cenro")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
